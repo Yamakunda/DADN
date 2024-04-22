8c03c16efd623242d6755e32450b412865041172
yamakunson <92131933+yamakunson@users.noreply.github.com>
2024-04-22 18:50:52 +0700

    22/4

--- src/screens/apiFacade.tsx
+++ src/screens/apiFacade.tsx
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 const PORT = 3000;
-const HOST = '192.168.171.178';
+const HOST = '192.168.1.5';
 
 export const apiFacade = {
-    async login(username: string, password: string) {
-        const res = await axios.post(`http://${HOST}:${PORT}/account/login`, {
-          username,
-          password,
-        });
-        return res.data;
-      },
-      async getRecord() {
-        const res = await axios.get(`http://${HOST}:${PORT}/record`);
-        return res.data;
-      },
+  async login(username: string, password: string) {
+    const res = await axios.post(`http://${HOST}:${PORT}/account/login`, {
+      username,
+      password,
+    });
+    return res.data;
+  },
+  async getRecord() {
+    const res = await axios.get(`http://${HOST}:${PORT}/record`);
+    return res.data;
+  },
   async switchLight(value: number) {
     return await axios.post(`http://${HOST}:${PORT}/record/store`, {
       light: value
@@ -35,5 +35,5 @@ export const apiFacade = {
     const tempdata = res.data.map((item: any) => Number(item.averageTemp));
     return { labels, humidata, tempdata };
   },
-  
+
 };
